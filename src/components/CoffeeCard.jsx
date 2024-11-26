53ff75136e51670cc8ec4f51011830fca67641b6
Sheikh Jabed <jabedbd2295@gmail.com>
2024-11-27 02:51:51 +0600

    Coffee Cards Done

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,34 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { IoEyeSharp } from "react-icons/io5";
+import { MdEdit } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
 
 const CoffeeCard = ({ coffee }) => {
-    const { _id, name, quantity, supplier, taste, photo } = coffee;
+    const { _id, name, chef, supplier, taste, photo } = coffee;
 
     return (
-      <div className="card card-side bg-base-100 shadow-xl">
-        
-          <img src={photo} alt="Movie" />
-        
-        <div className="flex justify-between w-full pr-4">
+      <div className="card lg:card-side bg-base-100 shadow-xl">
+        <figure>
+          <img src={photo} alt="coffee" />
+        </figure>
+        <div className="flex w-full m-4 items-center justify-between">
           <div>
-            <h2 className="card-title">Name: {name}</h2>
-            <p>{quantity}</p>
-            <p>{supplier}</p>
-            <p>{taste}</p>
+            <p>Name: {name}</p>
+            <p>Chef: {chef}</p>
+            <p>Taste: {taste}</p>
           </div>
-          <div className="card-actions justify-end">
-            <div className="btn-group btn-group-vertical space-y-4">
-              <button className="btn">View</button>
-              <Link to={`updateCoffee/${_id}`}>
-                <button className="btn">Edit</button>
-              </Link>
-              <button
-                // onClick={() => handleDelete(_id)}
-                className="btn bg-orange-500"
-              >
-                X
+          <div className="card-actions justify-end join join-vertical">
+            <button className="btn join-item">
+              View <IoEyeSharp />
+            </button>
+            <Link to={`/updateCoffee/${_id}`}>
+              <button className="btn join-item">
+                Edit <MdEdit />
               </button>
-            </div>
+            </Link>
+            <button
+              //   onClick={() => handleDelete(_id)}
+              className="btn join-item bg-red-500"
+            >
+              X <MdDelete />
+            </button>
           </div>
         </div>
       </div>
