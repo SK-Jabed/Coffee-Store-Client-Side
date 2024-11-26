f946bedaaa99f4a568c464a7420f8c3e9ec80461
Sheikh Jabed <jabedbd2295@gmail.com>
2024-11-26 23:27:10 +0600

    Add Coffee Functionality Completed

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -49,7 +49,7 @@ const AddCoffee = () => {
               It is a long established fact that a reader will be distraceted by
               the readable content of a page when looking at its layout. The
               point of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using Content here.
+              distribution of letters.
             </p>
           </div>
           <form onSubmit={handleAddCoffee} className="card-body">
