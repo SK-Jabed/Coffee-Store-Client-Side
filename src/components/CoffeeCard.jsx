f44ddfee6c2984a798cefd9ceb4727c2ea26e973
Sheikh Jabed <jabedbd2295@gmail.com>
2024-11-27 14:52:01 +0600

    Coffee Delete functionality Done

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,42 +1,73 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { data, Link } from "react-router-dom";
 import { IoEyeSharp } from "react-icons/io5";
 import { MdEdit } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
+import Swal from "sweetalert2";
 
 const CoffeeCard = ({ coffee }) => {
-    const { _id, name, chef, supplier, taste, photo } = coffee;
+  const { _id, name, chef, supplier, taste, photo } = coffee;
 
-    return (
-      <div className="card lg:card-side bg-base-100 shadow-xl">
-        <figure>
-          <img src={photo} alt="coffee" />
-        </figure>
-        <div className="flex w-full m-4 items-center justify-between">
-          <div>
-            <p>Name: {name}</p>
-            <p>Chef: {chef}</p>
-            <p>Taste: {taste}</p>
-          </div>
-          <div className="card-actions justify-end join join-vertical">
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+            method: "DELETE"
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your Coffee has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
+
+  return (
+    <div className="card lg:card-side bg-base-100 shadow-xl">
+      <figure>
+        <img src={photo} alt="coffee" />
+      </figure>
+      <div className="flex w-full m-4 items-center justify-between">
+        <div>
+          <p>Name: {name}</p>
+          <p>Chef: {chef}</p>
+          <p>Taste: {taste}</p>
+        </div>
+        <div className="card-actions justify-end join join-vertical">
+          <button className="btn join-item">
+            View <IoEyeSharp />
+          </button>
+          <Link to={`/updateCoffee/${_id}`}>
             <button className="btn join-item">
-              View <IoEyeSharp />
+              Edit <MdEdit />
             </button>
-            <Link to={`/updateCoffee/${_id}`}>
-              <button className="btn join-item">
-                Edit <MdEdit />
-              </button>
-            </Link>
-            <button
-              //   onClick={() => handleDelete(_id)}
-              className="btn join-item bg-red-500"
-            >
-              X <MdDelete />
-            </button>
-          </div>
+          </Link>
+          <button
+            onClick={() => handleDelete(_id)}
+            className="btn join-item bg-red-500"
+          >
+            X <MdDelete />
+          </button>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default CoffeeCard;
