6ed54b812158d871716171de709a1f5920530070
Sheikh Jabed <jabedbd2295@gmail.com>
2024-11-27 18:35:47 +0600

    Update Coffees New Route Added

--- src/components/UpdateCoffee.jsx
+++ src/components/UpdateCoffee.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const UpdateCoffee = () => {
+    const coffee = useLoaderData();
+
+    const { _id, name, chef, supplier, taste, photo } = coffee;
+
     return (
         <div>
-            <h2>Updated Coffees Here</h2>
+            <h2>Updated Coffee Here: {name}</h2>
         </div>
     );
 };
