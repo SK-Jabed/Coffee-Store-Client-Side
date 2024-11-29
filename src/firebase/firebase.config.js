aecc5e75f1b936a5af9a9bdef4cb0b735e06f6cf
Sheikh Jabed <jabedbd2295@gmail.com>
2024-11-29 19:14:28 +0600

    Auth Layout and Signup, SignIn Components Added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCw2zRIHA7tXNKIQMzCzBP4fkg0FZD_4OM",
+  authDomain: "coffee-store-ab754.firebaseapp.com",
+  projectId: "coffee-store-ab754",
+  storageBucket: "coffee-store-ab754.firebasestorage.app",
+  messagingSenderId: "833142643699",
+  appId: "1:833142643699:web:f666c4fc2fc74b52131086",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,6 +3,9 @@ import AddCoffee from "../components/AddCoffee";
 import UpdateCoffee from "../components/UpdateCoffee";
 import MainLayout from "../layouts/MainLayout/MainLayout";
 import HomePage from "../pages/HomePage";
+import AuthLayout from "../layouts/AuthLayout/AuthLayout";
+import SignUp from "../components/SignUp";
+import SignIn from "../components/SignIn";
 
 const router = createBrowserRouter([
   {
@@ -14,17 +17,31 @@ const router = createBrowserRouter([
         element: <HomePage></HomePage>,
         loader: () => fetch("http://localhost:5000/coffee"),
       },
+      {
+        path: "/addCoffee",
+        element: <AddCoffee></AddCoffee>,
+      },
+      {
+        path: "/updateCoffee/:id",
+        element: <UpdateCoffee></UpdateCoffee>,
+        loader: ({params}) => fetch(`http://localhost:5000/coffee/${params.id}`),
+      },
     ],
   },
   {
-    path: "/addCoffee",
-    element: <AddCoffee></AddCoffee>,
-  },
-  {
-    path: "/updateCoffee/:id",
-    element: <UpdateCoffee></UpdateCoffee>,
-    loader: ({params}) => fetch(`http://localhost:5000/coffee/${params.id}`),
-  },
+    path: "/auth",
+    element: <AuthLayout></AuthLayout>,
+    children: [
+      {
+        path: "/auth/signUp",
+        element: <SignUp></SignUp>
+      },
+      {
+        path: "/auth/signIn",
+        element: <SignIn></SignIn>
+      }
+    ]
+  }
 ]);
 
 export default router;
